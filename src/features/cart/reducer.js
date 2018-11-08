5156ffba7320d689a98c7e499e49142acf138c68
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-11-07 23:57:44 -0800

    started on part 2. forgot to switch branches. doing so now

--- src/features/cart/reducer.js
+++ src/features/cart/reducer.js
@@ -1,3 +1,9 @@
+const cartWithoutItem = (cart, item) => cart.filter(cartItem => cart.item !== item.id) //this makes sure we dont have duplicates in our cart. Making sure Each cart.item doesn't have the same ID
+const itemInCart = (cart, item) => cart.filter(cartItem => cart.item === item.id)[0] //this grabs the first index of the array rather than grabbing the entire array
+const addToCart = (cart, item) => {
+  const cartItem = itemInCart(cart, item)
+}
+
 const cartReducer = (state=[], action) => { //passing in initial state of an empty array
   switch(action.type) {
     case 'ADD':
