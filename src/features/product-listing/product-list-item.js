d806404d9e410a153066774e4ffb7c812e79912e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-01-15 17:50:40 -0800

    added some more styling in part one video stopped at 34 minutes

--- src/features/product-listing/product-list-item.js
+++ src/features/product-listing/product-list-item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ProductListItem(props) {
   return <div className='product-list-item'>
-    <h3>{props.name}</h3>
+    <h3>{props.product.name}</h3>
     <img
       height={ 100 }
       title={ props.product.name }
