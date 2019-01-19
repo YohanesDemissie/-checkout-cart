d91be1c1e93e57956bf80a7782e2dc859f730e80
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-01-18 23:58:48 -0800

    need to review part 2 final for compare and contrast to make cart stop shuffling

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-function sort(items) {
-  return items.sort(( a, b) => a.id < b.id) //a nd b are 0 and first index
+// *** TRY TAKING IT OUT OF TABLE ELEMENTS AND INTO REGULAR ELEMENTS. TABBBLES MIGHT HAVE AUYTOMATIC WAYS OF ORGAINZING 
+
+const sort = (items) => {
+  return items.sort((a, b) => a.id < b.id)
 }
 
 function Cart(props) {
   return <table>
-      <thead>
-        <tr>
-          <th> Item </th>
-          <th> Quantity </th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          sort(props.cart).map(item => <tr>
-            <td>{item.name}</td>
-            <td>{item.quantity}</td>
-            <td>
-              <button onClick={() => props.addToCart(item)}
-              >+</button>
-
-              <button onClick={() => props.removeFromCart(item)}
-              >-</button>
-            </td>
-            <td>
-              <button onClick={() => props.removeAllFromCart(item)}
-                >Remove All From Cart
-              </button>
-            </td>
-          </tr>)
-        }
-      </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Quantity</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        sort(props.cart).map(item => <tr>
+          <td>{item.name}</td>
+          <td>{item.quantity}</td>
+          <td>
+            <button
+              onClick={(e) => props.addToCart(item)}
+            >+</button>
+            <button
+              onClick={(e) => props.removeFromCart(item)}
+            >-</button>
+          </td>
+          <td>
+            <button
+              onClick={(e) => props.removeAllFromCart(item)}
+            >Remove from cart</button>
+          </td>
+        </tr>)
+      }
+    </tbody>
+  </table>
 }
 
 function mapStateToProps(state) {
@@ -47,15 +50,103 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     addToCart: (item) => {
-      dispatch({type: 'ADD', payload: item})
+      dispatch({ type: 'ADD', payload: item })
     },
     removeFromCart: (item) => {
-      dispatch({type: 'REMOVE', payload: item})
+      dispatch({ type: 'REMOVE', payload: item })
     },
     removeAllFromCart: (item) => {
       dispatch({ type: 'REMOVE_ALL', payload: item })
-    },
+    }
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cart) // connected through redux with "mapStateToProps function and "connect" method to call "mapStateToProps" passing in (Cart)
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sort(items) {
+//   return items.sort((a, b) => a.id < b.id )
+// }
+// // function sort(items) {
+// //   return items.sort(( a, b) => a.id < b.id) //a nd b are 0 and first index
+// // }
+
+// function Cart(props) {
+//   return <table>
+//       <thead>
+//         <tr>
+//           <th> Item </th>
+//           <th> Quantity </th>
+//           <th></th>
+//           <th></th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {
+//           sort(props.cart).map(item => <tr>
+//             <td>{item.name}</td>
+//             <td>{item.quantity}</td>
+//             <td>
+//               <button onClick={(props) => props.addToCart(item)}
+//               >+</button>
+
+//               <button onClick={(props) => props.removeFromCart(item)}
+//               >-</button>
+//             </td>
+//             <td>
+//               <button onClick={(props) => props.removeAllFromCart(item)}
+//                 >Remove All From Cart
+//               </button>
+//             </td>
+//           </tr>)
+//         }
+//       </tbody>
+//     </table>
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     cart: state.cart,
+//   }
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     addToCart: (item) => {
+//       dispatch({type: 'ADD', payload: item})
+//     },
+//     removeFromCart: (item) => {
+//       dispatch({type: 'REMOVE', payload: item})
+//     },
+//     removeAllFromCart: (item) => {
+//       dispatch({ type: 'REMOVE_ALL', payload: item })
+//     },
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Cart) // connected through redux with "mapStateToProps function and "connect" method to call "mapStateToProps" passing in (Cart)
